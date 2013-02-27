f598c82fb88d8b644abf469d3fb688f008343a7f
Joel Clark <jclarkvr@gmail.com>
2013-02-27 18:59:50 +0000

    Initial Commit

---
+++ BinaryAdder/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  BinaryAdder
+//
+//  Created by Joel Clark on 27/02/2013.
+//  Copyright (c) 2013 Joel Clark. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
